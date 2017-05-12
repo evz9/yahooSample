126690e612df772a9991d020380da55dabf449a4
evz9 <evz@u.northwestern.edu>
2017-05-12 12:10:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,16 @@ def webhook():
     # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
-    return r
+    return  Headers:
+   Content-type: application/json
+   Body:
+    {
+    "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
+    "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+    "data": {...},
+    "contextOut": [...],
+    "source": "DuckDuckGo"
+    }
 
 
 def processRequest(req):
